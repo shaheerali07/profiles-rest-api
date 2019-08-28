e68ce48568fb4490e78aacfb998bd0f09dbf5579
shery <ashaheer606@gmail.com>
2019-08-28 21:48:40 +0500

    Added POST, PUT, PATCH and DELETE methods to our APIView

--- profiles_api/migrations/0001_initial.py
+++ profiles_api/migrations/0001_initial.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0011_update_proxy_permissions'),
+        ('auth', '0008_alter_user_username_max_length'),
     ]
 
     operations = [
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 #from rest_framework.renderers import JSONRenderer
+
+from . import serializers
 # Create your views here.
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     #renderer_classes = [JSONRenderer]
 
+    #get request
     def get(self, request, format=None):
         """Returns a list of API View features"""
 
@@ -21,3 +26,34 @@ class HelloApiView(APIView):
 
         ]
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    #post request
+    def post(self, request):
+        """Create a hello message with out name"""
+        serializer = self.serializer_class(data= request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+    #put request is used to update a text
+    #here arguement pk means primary key and it is used to update the selected id object
+    def put(self, request, pk=None):
+        """Handle updating an objext"""
+        return Response({'method':'PUT'})
+
+    #patch request is used to partial update the object
+    #for example if u want to update the last name but not the first name then u use patch request to partially update an object
+    def patch(self, request, pk=None):
+        """Handle a partial update of object"""
+        return Response({'method':'PATCH'})
+
+    #Delete request is used to delete an objects in the database
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
