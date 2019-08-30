bb7f8bfce402632a0410598f0495128263a6e1ed
shery <ashaheer606@gmail.com>
2019-08-30 18:21:31 +0500

    Added profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,31 @@
 from rest_framework import serializers
+from . import models
 
 class HelloSerializer(serializers.Serializer):
     """Serializer a name field for testing our APIView"""
 
     name = serializers.CharField(max_length = 10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        #To make password visible only when object is created not in viewing object
+        extra_kwargs = {
+            'password': {
+                #'last_login': {'read_only': True},
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """create and return a new user"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,9 +3,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 #from rest_framework.renderers import JSONRenderer
 
 from . import serializers
+from . import models
+from . import permissions
 # Create your views here.
 
 
@@ -102,3 +106,11 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method':'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
